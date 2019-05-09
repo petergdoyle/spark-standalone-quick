abf9eb87339c8601b9a2cfb1f766e654b5a0cf4e
petergdoyle <peter.doyle@travelport.com>
2019-05-09 10:03:21 -0600

    work to date

--- spark_standup.sh
+++ spark_standup.sh
@@ -1,2 +1,11 @@
 #!/usr/bin/env bash
 . ./spark_common.sh
+
+define_cluster_nodes
+
+master=$(cat spark-cluster.info | grep MASTER_NODE |egrep -oh '[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}')
+echo "master: $master"
+workers=$(cat spark-cluster.info | grep WORKER_NODE |egrep -oh '[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}')
+for worker in $workers; do
+  echo "worker: $worker"
+done
